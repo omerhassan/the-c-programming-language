62a672c09b94325b3d5ed1ab929bca2771444df5
omer <omerhassan688@gmail.com>
2019-12-13 19:51:03 +0300

    Create Exercise 2-9.c

---
+++ Chapter 2. Types, Operators, and Expressions/Exercise 2-9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Exercise 2-9. In a two's complement number system, x &= (x-1) deletes the
+   rightmost 1-bit in x. Explain why. Use this observation to write a faster ver-
+   sion of bit count. */
+int bitcount(unsigned x)
+{
+    int b;
+
+    for (b = 0; x != 0; x &= (x-1))
+        b++;
+    return b;
+}
